32ed9e94b87dd7d5837b3b08434ef43a706d2a1e
NatalyLC <natalymlc@hotmail.com>
2025-12-30 10:37:59 -0500

    Edicion artProg

--- Js/articulosProg.js
+++ Js/articulosProg.js
@@ -15,45 +15,84 @@ const programas = {
     },
     "cordova-un-general": {
         titulo: "Córdova, un General Llamado Arrojo",
-        descripcion: "Historia del General Córdova y su impacto en la historia del país.",
+        descripcion: `
+            Es una miniserie que narra la vida y carrera del General Antonio José de Córdova, una figura prominente en la historia militar de América Latina. La serie se enfoca en sus hazañas y liderazgo durante la independencia de los países sudamericanos, retratando cómo Córdova jugó un papel crucial en las luchas por la liberación del dominio colonial. Con una narrativa apasionante, la miniserie ofrece una visión detallada de sus estrategias militares, sus relaciones con otros líderes de la independencia, y los desafíos que enfrentó en su misión por la libertad.
+            \nA través de una combinación de dramatización histórica y elementos documentales, “Córdova, un General Llamado Arrojo” explora la personalidad compleja y el carácter indomable de Córdova. La serie no solo celebra sus victorias y contribuciones a la causa independentista, sino que también ofrece una reflexión sobre su impacto duradero en la historia y la cultura de los países liberados. Su retrato en la miniserie busca capturar la esencia de un líder audaz y comprometido, cuya valentía y determinación dejaron una marca indeleble en la historia de América Latina.
+            \nSumérgete en la vida y legado de uno de los grandes héroes de la historia. Sintoniza el programa en el siguiente horario:
+        `,
         imagen: "../Imagenes/p2.png",
         horario: "Lunes a Viernes: 21:00 a 22:00"
     },
     "debora-mujer-colombia": {
         titulo: "Débora la mujer que desnudó a Colombia",
-        descripcion: "Análisis del programa y entrevistas con los protagonistas.",
+        descripcion:`
+            Es una miniserie transmitida por GamaTV que explora la vida y el impacto de Débora Arango, una destacada artista colombiana cuya obra revolucionó el arte y la sociedad en Colombia durante el siglo XX. A través de una narrativa envolvente y detallada, la miniserie ofrece un retrato íntimo de Arango, destacando su valentía y su desafío a las normas sociales y culturales de su tiempo. La historia aborda cómo sus obras, que frecuentemente contenían temáticas controvertidas y críticas hacia la corrupción y la desigualdad, provocaron debates intensos y ayudaron a cambiar la percepción del arte y el rol de la mujer en la sociedad colombiana.
+            \nLa miniserie no solo se centra en la carrera artística de Débora Arango, sino también en su vida personal, sus luchas y su legado duradero. Con una producción cuidadosa que combina dramatización y elementos documentales, “Débora, la mujer que desnudo a Colombia” ofrece una visión profunda de cómo Arango desafió las convenciones y dejó una marca indeleble en el arte y la cultura de su país. Su influencia sigue siendo relevante, y la miniserie celebra su contribución a la transformación social y cultural en Colombia, proporcionando una reflexión sobre el poder del arte para cuestionar y redefinir las normas establecidas.
+            \nDescubre la fascinante historia de Débora y su impacto en la sociedad colombiana. No te pierdas cada episodio en el siguiente horario:
+        `,
         imagen: "../Imagenes/p3.jpg",
         horario: "Lunes a Viernes: 21:00 a 22:00"
     },
     "virgencita-ecuador": {
         titulo: "La Virgencita Ecuador",
-        descripcion: "Historia del General Córdova y su impacto en la historia del país.",
-        imagen: "../Imagenes/p2.png",
-        horario: "Lunes a Viernes: 21:00 a 22:00"
+        descripcion:`
+            “La Virgencita Ecuador” es un programa de televisión ecuatoriano que se ha ganado un lugar especial en el corazón de sus espectadores gracias a su enfoque único en temas de devoción y espiritualidad. El programa se centra en la figura de la Virgen María y en el papel que juega en la vida de los ecuatorianos, explorando cómo las creencias religiosas y la fe influencian la cultura y las tradiciones locales. A través de reportajes, testimonios “La Virgencita Ecuador” ofrece una perspectiva profunda y enriquecedora sobre la devoción mariana en el país.
+            \nEn cada episodio, el programa presenta historias conmovedoras de devotos que comparten sus experiencias personales y los milagros que atribuyen a la Virgen María. Además de estos relatos, “La Virgencita Ecuador” brinda a los espectadores una visión completa de las manifestaciones de fe en diferentes regiones del Ecuador. La producción del programa se distingue por su respeto hacia las tradiciones religiosas y su habilidad para capturar la esencia de la espiritualidad ecuatoriana de manera auténtica.
+            \n“La Virgencita Ecuador” no solo se limita a la exploración de la fe y la devoción, sino que también aborda temas relacionados con la vida comunitaria y el impacto positivo de la religión en la sociedad. Con un enfoque en las historias humanas y los valores compartidos, el programa busca fomentar una mayor comprensión y apreciación de la espiritualidad en el contexto ecuatoriano. Su capacidad para conectar con los sentimientos y creencias de la audiencia hace de “La Virgencita Ecuador” una celebración de la fe y la cultura que resuena profundamente en la vida cotidiana de sus espectadores.
+            \nSumérgete en la espiritualidad y la devoción ecuatoriana a través de este cautivador programa. No te pierdas cada episodio en el siguiente horario:
+        `,
+        imagen: "../Imagenes/p4.jpg",
+        horario: "Lunes a Viernes: 20:00 a 21:00"
     },
     "puro-teatro": {
         titulo: "Puro Teatro",
-        descripcion: "Análisis del programa y entrevistas con los protagonistas.",
-        imagen: "../Imagenes/p3.jpg",
-        horario: "Lunes a Viernes: 21:00 a 22:00"
+        descripcion:`
+            Es un programa de comedia ecuatoriano que se destaca por su ingenio y creatividad al ofrecer una variedad de sketches y actuaciones humorísticas. Con un enfoque en el entretenimiento y la risa, el programa presenta una serie de escenas cómicas que abarcan desde situaciones cotidianas hasta parodias de la vida política y social. Cada episodio está diseñado para captar la atención del público con guiones bien elaborados y actuaciones vibrantes, haciendo de “Puro Teatro” una fuente constante de diversión y alegría para los espectadores.
+            \nEl programa se caracteriza por su capacidad para abordar temas relevantes y actuales con un toque humorístico, utilizando personajes carismáticos y situaciones absurdas para reflejar aspectos de la cultura y la sociedad ecuatoriana. Los guionistas y actores de “Puro Teatro” combinan sátira y comedia para ofrecer un contenido que no solo entretiene, sino que también invita a la reflexión sobre diversos aspectos de la vida cotidiana y las costumbres locales. La variedad de personajes y escenarios permite que el programa mantenga un ritmo dinámico y atractivo a lo largo de cada emisión.
+            \nSe ha consolidado como un referente en la comedia ecuatoriana gracias a su originalidad y capacidad para conectar con la audiencia. Su éxito radica en la habilidad para presentar situaciones y personajes que resuenan con el público, combinando humor inteligente con una crítica social aguda. A través de su formato innovador y su estilo entretenido, “Puro Teatro” sigue siendo una de las opciones preferidas para aquellos que buscan disfrutar de un buen rato y reírse de las situaciones más cotidianas y surrealistas de la vida.
+            \nDisfruta de la comedia ecuatoriana en el siguiente horario:
+        `,
+        imagen: "../Imagenes/p5.jpg",
+        horario: `
+            Lunes a Viernes: 16:30 a 17:30 \nSábados: 20:00 a 21:00
+        `
     },
     "berracas": {
         titulo: "Berracas",
-        descripcion: "Análisis del programa y entrevistas con los protagonistas.",
-        imagen: "../Imagenes/p3.jpg",
-        horario: "Lunes a Viernes: 21:00 a 22:00"
+        descripcion:`
+            Es un programa documental transmitido por GamaTV que se sumerge en las vidas de mujeres excepcionales que han superado grandes obstáculos en sus comunidades. A través de una narrativa impactante y detallada, el programa destaca las historias de mujeres valientes que enfrentan desafíos significativos, desde problemas sociales hasta adversidades personales, con una fortaleza y determinación admirables. Cada episodio ofrece una visión profunda de su lucha y resiliencia, revelando el impacto de sus experiencias en sus vidas y en las de quienes las rodean.
+            \nEl programa se caracteriza por su enfoque en la autenticidad y el respeto hacia sus protagonistas, utilizando un estilo de cinematografía que captura la esencia de sus historias con sensibilidad y precisión. “Las Berracas” combina entrevistas íntimas, secuencias de la vida cotidiana y reportajes en profundidad para ofrecer una imagen completa de las mujeres que protagonizan el documental. Esta combinación de elementos visuales y narrativos permite a los espectadores conectar emocionalmente con las historias y comprender mejor las realidades que enfrentan estas mujeres.
+            \nAdemás de mostrar las historias personales de las protagonistas, “Las Berracas” también aborda temas más amplios relacionados con el empoderamiento femenino, la justicia social y el cambio cultural. El programa no solo busca inspirar y educar, sino también promover la reflexión sobre el papel crucial que juegan estas mujeres en la transformación de sus comunidades y en la lucha por un mundo más equitativo. Con su enfoque en la fuerza y la resiliencia femenina, “Las Berracas” se establece como un homenaje poderoso a las mujeres que desafían las normas y luchan por un futuro mejor.
+            \nExplora las historias inspiradoras de mujeres valientes y fuertes que están cambiando el mundo. Sintoniza en el siguiente horario:
+        `,
+        imagen: "../Imagenes/p6.jpg",
+        horario: `
+            Lunes a Viernes: 15:30 a 16:30 \nSábados y Domingos: 16:00 a 18:30
+        `
     },
     "protagonistas": {
         titulo: "Los Protagonistas",
-        descripcion: "Historia del General Córdova y su impacto en la historia del país.",
-        imagen: "../Imagenes/p2.png",
-        horario: "Lunes a Viernes: 21:00 a 22:00"
+        descripcion:`
+            Es un destacado programa de deportes en Ecuador que se enfoca en ofrecer una cobertura completa de los eventos deportivos más importantes tanto a nivel nacional como internacional. Con un enfoque en el fútbol, uno de los deportes más populares en el país, el programa proporciona a sus espectadores análisis detallados de los partidos, entrevistas con jugadores y entrenadores, y reportajes especiales sobre equipos y ligas. Además de su cobertura del fútbol ecuatoriano, “Los Protagonistas” también incluye secciones dedicadas a otros deportes y eventos deportivos globales, brindando una visión integral del panorama deportivo.
+            \nEl programa es conocido por su capacidad para ofrecer contenido dinámico y actualizado, capturando la esencia de la pasión deportiva que caracteriza a los aficionados ecuatorianos. Los presentadores y analistas de “Los Protagonistas” aportan una combinación de conocimiento experto y entusiasmo, lo que permite a los espectadores disfrutar de una experiencia informativa y entretenida. Las entrevistas exclusivas y los reportajes en profundidad destacan tanto los triunfos como los desafíos de los protagonistas del deporte, ofreciendo una perspectiva completa sobre el impacto de estos eventos en los deportistas y los seguidores.
+            \n“Los Protagonistas” no solo se limita a cubrir los eventos deportivos, sino que también se involucra en la discusión sobre temas relevantes en el ámbito deportivo, como el desarrollo de jóvenes talentos, la gestión de clubes y las políticas deportivas. Con su enfoque en ofrecer análisis y contenido de calidad, el programa se ha consolidado como una fuente clave de información y entretenimiento para los aficionados al deporte en Ecuador. Su capacidad para conectar con la audiencia y proporcionar una cobertura exhaustiva lo convierte en un referente en el ámbito de los programas deportivos del país.
+            \nMantente al tanto de toda la acción y noticias del deporte ecuatoriano con nuestro emocionante programa. Disfruta de los mejores análisis, entrevistas y resúmenes en el siguiente horario:
+        `,
+        imagen: "../Imagenes/p7.png",
+        horario: `
+            Lunes a Viernes: 14:00 a 14:30 y 23:00 a 23:30 \nDomingos: 18:30 a 19:00
+        `
     },
     "dw-noticias": {
         titulo: "DW noticias",
-        descripcion: "Análisis del programa y entrevistas con los protagonistas.",
-        imagen: "../Imagenes/p3.jpg",
-        horario: "Lunes a Viernes: 21:00 a 22:00"
+        descripcion:`
+            Es un programa informativo de Deutsche Welle (DW) que se encarga de proporcionar una cobertura detallada de las noticias internacionales, complementando el noticiero Gamanoticias. Después de la emisión de Gamanoticias, que ofrece un enfoque general de los acontecimientos del día, “DW Noticias” se centra en ofrecer a los espectadores una visión más profunda y exhaustiva de los eventos globales. El programa está diseñado para mantener a la audiencia bien informada sobre los desarrollos internacionales más importantes y sus repercusiones a nivel mundial.
+            \nLa estructura de “DW Noticias” permite un análisis detallado de las noticias internacionales, abordando temas que van desde la política y la economía hasta eventos sociales y culturales. Con un equipo de corresponsales y reporteros ubicados en diversas partes del mundo, el programa asegura una cobertura amplia y diversa, brindando a los espectadores una perspectiva global de los acontecimientos. Además, “DW Noticias” incluye entrevistas con expertos y análisis de las tendencias emergentes, lo que permite una comprensión más completa de las noticias internacionales.
+            \nEl enfoque de “DW Noticias” en las noticias internacionales hace que sea una fuente valiosa para quienes buscan mantenerse al día con los acontecimientos globales más relevantes. Con su programación que sigue al noticiero Gamanoticias, el programa ofrece una transición natural hacia una cobertura más específica y detallada de los eventos mundiales, asegurando que los espectadores reciban la información más precisa y actualizada sobre el panorama internacional.
+            \nMantente informado con las últimas noticias y reportajes internacionales que impactan al mundo. No te pierdas nuestro programa en el siguiente horario:
+        `,
+        imagen: "../Imagenes/p8.jpg",
+        horario: "Lunes a Viernes: 13:30 a 14:00"
     }
 };
 
